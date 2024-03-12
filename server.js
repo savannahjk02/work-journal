91fac92fdb584a4ad80265840e30e79c8574007f
savannahjk02 <savannahjk02@gmail.com>
2024-03-11 21:55:31 -0400

    deleted Develop folder for deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./Develop/routes/apiRoutes');
+const htmlRoutes = require('./Develop/routes/htmlRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
